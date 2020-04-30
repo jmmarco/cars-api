6ce857a398826829bf17154502c03eab8d6d246a
Juan Marco <jmmarco@gmail.com>
2020-04-30 13:57:56 -0300

    Update all relevant routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'cars/index'
-      get 'cars/create'
-      get 'cars/show'
-      get 'cars/destroy'
+      get 'cars', to: 'cars#index'
+      post 'cars', to: 'cars#create'
+      get 'cars/:id', to: 'cars#show'
+      put 'cars/:id', to: 'cars#update'
+      delete 'cars/:id', to: 'cars#destroy'
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
